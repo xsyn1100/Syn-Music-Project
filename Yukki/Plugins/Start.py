ce64004be6347debe9708a46c2b2cb4397630bd5
Yansen <97857681+KyoukaXP@users.noreply.github.com>
2022-03-25 13:41:13 +0700

    Update Start.py

--- Yukki/Plugins/Start.py
+++ Yukki/Plugins/Start.py
@@ -78,7 +78,7 @@ async def welcome(_, message: Message):
             return
 
 
-@app.on_message(filters.command(["help", "start"]) & filters.group)
+@app.on_message(filters.command(["mhelp", "mstart"]) & filters.group)
 @PermissionCheck
 async def useradd(_, message: Message):
     out = start_pannel()
@@ -91,7 +91,7 @@ async def useradd(_, message: Message):
     )
 
 
-@app.on_message(filters.command("settings") & filters.group)
+@app.on_message(filters.command("msettings") & filters.group)
 @PermissionCheck
 async def settings(_, message: Message):
     c_id = message.chat.id
